1b3718c6fb5c257d33d84a654362beaea29b6f95
Sandy Demian <sdemian@knights.ucf.edu>
2024-07-26 20:27:43 -0400

    more updates

--- responsive.js
+++ responsive.js
@@ -1,24 +1,23 @@
 // Sandy Demian
 // responsive.js
 
-
 profilePic = document.getElementById('profilePic');
 
+mobileRow = document.getElementById('mobileRow');
 resumeL = document.getElementById('resumeL');
 
 window.addEventListener("load", function () 
 {
   if (window.innerWidth < 960) 
   { 
-    //if (mobileImg) mobileImg.style.display = 'block';
-    //if (webImg) webImg.style.display = 'none';
-    if (profilePic) profilePic.width = 20%;
+    if (profilePic)
+    {
+      profilePic.style.width = '50%';
+    }
 
-    if (resumeL) resumeL.width = '30px';
-  }
-  else
-  {
-    //if (mobileImg) mobileImg.style.display = 'none';
-    //if (webImg) webImg.style.display = 'block';
+    if (mobileRow)
+    {
+      mobileRow.appendChild(resumeL);
+    }
   }
 });
